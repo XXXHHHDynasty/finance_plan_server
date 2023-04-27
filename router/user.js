d847ec820d3f47d33dad2f815e2e50949b6d6344
jolucena <64047524+jolucena@users.noreply.github.com>
2023-04-27 16:28:42 -0400

    Update user.js

--- router/user.js
+++ router/user.js
@@ -6,9 +6,9 @@ const expressJoi = require('@escook/express-joi')
 const { reg_login_schema } = require('../schema/user')
 
 // Register a new user
-router.post('/reguser', expressJoi(reg_login_schema), userHandler.regUser)
+router.post('/reguser', (expressJoi(reg_login_schema), userHandler.regUser))
 
 // Log in
-router.post('/login', expressJoi(reg_login_schema), userHandler.login)
+router.post('/login', (expressJoi(reg_login_schema), userHandler.login))
 
 module.exports = router
